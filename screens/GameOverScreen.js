a1f45cfd73e009bc8410c465f36bbb0b2bc2abf1
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-13 01:15:05 +0300

    dealt more with the text component by applying different properties.

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -4,6 +4,8 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native'
 import TitleText from '../components/TitleText'
 import BodyText from '../components/BodyText'
 
+import Colours from '../constants/colours'
+
 
 const GameOverScreen = props => {
   return (
@@ -11,13 +13,12 @@ const GameOverScreen = props => {
       <TitleText>Game Over!!!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          // source={require('../assets/images/success.png')}
-          source={{uri:'https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_1280.jpg'}}
+          source={require('../assets/images/success.png')}
+          // source={{uri:'https://cdn.pixabay.com/photo/2015/04/12/17/51/dominoes-719199_1280.jpg'}}
           style={styles.image}
           resizeMode="cover" />
       </View>
-      <BodyText>Number of Attempts: {props.attemptsCount} </BodyText>
-      <BodyText>Number was: {props.userNumber} </BodyText>
+      <View style={styles.resultsContainer}><BodyText style={styles.resultText}>Your phone needed <Text style={styles.highlight}>{props.attemptsCount}</Text> attempts to guess the number <Text style={styles.highlight}>{props.userNumber}</Text></BodyText></View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   )
@@ -36,12 +37,24 @@ const styles = StyleSheet.create({
     borderRadius: 150,
     borderWidth: 3,
     borderColor: 'black',
-    overflow:'hidden',
-    marginVertical:30
+    overflow: 'hidden',
+    marginVertical: 20
   },
   image: {
     width: '100%',
     height: '100%'
+  },
+  resultsContainer: {
+    marginHorizontal: 15,
+    marginVertical: 15
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20
+  },
+  highlight: {
+    color: Colours.primary,
+    fontFamily: 'open-sans-bold'
   }
 })
 
