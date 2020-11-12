6e1431336c417faf648de79e524334396848285f
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-12 14:26:13 +0300

    loaded image from the network

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -11,7 +11,8 @@ const GameOverScreen = props => {
       <TitleText>Game Over!!!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          source={require('../assets/images/success.png')}
+          // source={require('../assets/images/success.png')}
+          source={{uri:'https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_1280.jpg'}}
           style={styles.image}
           resizeMode="cover" />
       </View>
