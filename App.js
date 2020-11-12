87a659a2ab8019c9f86af063537f00e8883acca1
Lawrence Macharia <macharialawrence2011@gmail.com>
2020-11-12 07:42:02 +0300

    Added custom font

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font'
+import { AppLoading } from 'expo'
 
 import Header from './components/Header';
 import StartScreen from './screens/StartScreen'
@@ -9,7 +10,7 @@ import GameScreen from './screens/GameScreen'
 import GameOverScreen from './screens/GameOverScreen'
 
 const fetchFonts = () => {
-  Font.loadAsync({
+  return Font.loadAsync({
     'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
     'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   })
@@ -19,7 +20,15 @@ export default function App() {
 
   const [userNumber, setUserNumber] = useState()
   const [attemptsCount, setAttemptsCount] = useState(0)
+  const [dataLoaded, setDataLoaded] = useState(false)
 
+  if (!dataLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => setDataLoaded(true)}
+      onError={(err) => console.log(err)}
+    />
+  }
 
   const configureNewGameHandler = () => {
     setAttemptsCount(0)
